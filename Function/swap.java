ef0a62d8fd9c8c38c87cfc3b466051120252b2aa
himanshu-maano <himanshu.kumarchy3@gmail.com>
2024-08-27 00:14:34 +0530

    cg

---
+++ Function/swap.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class swap {
+    public static void Swap(int a, int b) {
+        // Swap
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = "+a);
+        System.out.println()"b = "+b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enetr a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter b : ");
+        int b = sc.nextInt();
+        Swap(a,b);
+        sc.close();
+    }
+}
--- Loop/Armstrong.java
+++ Loop/Armstrong.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class prac {
+public class Armstrong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter num : ");
