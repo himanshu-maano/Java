3f8ad554c65b1b229305949cba4afaac56c565d2
himanshu-maano <himanshu.kumarchy3@gmail.com>
2024-08-27 00:21:49 +0530

    change

--- Function/basicFunction.java
+++ Function/basicFunction.java
@@ -1,13 +1,13 @@
 
 public class basicFunction {
-    public static int Greet() {
+    public static int greet() {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
         return 0;
     }
     public static void main(String[] args) {
-        Greet();
+        greet();
     }
 }
 
--- Function/swap.java
+++ Function/swap.java
@@ -7,7 +7,7 @@ public class swap {
         a = b;
         b = temp;
         System.out.println("a = "+a);
-        System.out.println()"b = "+b);
+        System.out.println("b = "+b);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
